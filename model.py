d6a9eae072a276ab0533dee78ea554e8f3eeb86b
Daniele <daniele.rb6@gmail.com>
2021-06-30 17:11:52 +0200

    feat: Add first model

--- model.py
+++ model.py
@@ -1,3 +1,7 @@
+import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
 import csv
 import cv2
     
@@ -15,11 +19,7 @@ measurements = []
 
 for line in lines:
     # Read the path to the center image. 
-    img_path = line[0]
-    # Split the path and get the tokens.  
-    tokens = img_path.split('/')
-    # Get the last token (the filename).
-    filename = tokens[-1]
+    filename = line[0]
     # Build the local path to the image.  
     local_path = './data/' + filename
     # Read the image and append ot to the list. 
@@ -30,6 +30,18 @@ for line in lines:
     measurement = line[3]
     measurements.append(measurement)
     
-print(len(images))
-print(len(measurements))
-    
+# Create the training and label Numpy arrays. 
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+#print(X_train.shape)
+model = Sequential()
+# Pass the X_train dimensions as input shape.
+model.add(Flatten(input_shape=(160, 320,3)))
+# As we're doing regression here, we only need 1 output. 
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss='mse')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+
+model.save('model.h5')
